7ae3a97a6c34ee1ca8102f1a766c18cc592d5cf3
Rana Saifullah <111068945+Rana-Saif-ullah@users.noreply.github.com>
2023-09-11 12:17:01 +0500

    completed all the website

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var menuicon = document.getElementById("menu__btn");
+var navicon = document.getElementById("navlinks");
+
+menuicon.addEventListener("click", function(){
+    navicon.classList.toggle("activenav")
+})
